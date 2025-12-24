50285b14971d2d5df7de63437fe65bacb1fe8751
Sneha-162 <160725837+Sneha-162@users.noreply.github.com>
2025-12-24 11:38:29 +0000

    [LeetCode Sync] Runtime - 2208 ms (5.03%), Memory - 28.1 MB (90.71%)

--- my-folder/0169-majority-element/solution.cpp
+++ my-folder/0169-majority-element/solution.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n= nums.size();
-        for(int val:nums){
-            int freq = 0;
-            for(int el:nums){
-                if(el==val){
-                    freq++;
-
+         for(int i=0;i<nums.size();i++){
+            int cnt = 0;
+            for(int j=0;j<nums.size();j++){
+                if(nums[i]==nums[j]){
+                    cnt++;
                 }
-
+            }
+            if(cnt>nums.size()/2){
+                return nums[i];
 
             }
-            if(freq>n/2){
-                return val;
-            }
+            
         }
         return -1;
-        
     }
-    
 };
